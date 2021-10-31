7bbc57b4aa911b6e15110a3c4858bc3d7aace860
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-10-31 21:13:54 +0500

    initial commit

---
+++ C++ (Hard practice questions)/out.cpp
@@ -0,0 +1,26 @@
+/* Program that produce output of the form                     */
+
+#include <iostream>
+using namespace std;
+
+void encrypt(char T[])
+{
+    for (int i = 0; T[i] != '\0'; i += 2)
+
+    if (T[i] == 'A' || T[i] == 'E')
+        T[i] = '#';
+    else if (islower(T[i]))
+        T[i] = toupper(T[i]);
+    else
+        T[i] = '@';
+}
+
+int main()
+{
+    char text[]="SaVE EArtH";
+    encrypt(text);
+
+    cout << text << endl;
+    
+    return 0;
+}
